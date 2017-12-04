f4dcc7519af3d9fabee38e6e3be2ca9686676176
sungpado <34066247+sungpado@users.noreply.github.com>
2017-12-04 18:46:26 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'MzIxNjY0MTY2NDkyMzcyOTky.DQZt1w.279PjUHKpslZw5UzR3zik44p0Gw';
 
 exports.PREFIX = '~';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyCY7DSGpbHsJv0gTSdL71VTCYhMrn6PRF0';
